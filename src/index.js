c01de1b52f985f5d3005a58af46c00a9746957ab
Weff182 <weffomax@gmail.com>
2021-02-27 18:33:45 +0300

    feat: added new functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
-}
+    if (n < 0) {
+        n = n*-1;
+        let reversed = String(n).split('').reverse().join(''); 
+    }
+    let reversed = String(n).split('').reverse().join(''); 
+    let result = Number(reversed);
+    return result;
+ }
+
